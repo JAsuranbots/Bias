98e77a0e5d7a3fc45e8a08ee189fea3195c3b4da
infinitrocyber <55886074+Nirmalraj10567@users.noreply.github.com>
2023-05-06 06:20:21 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,51 +1,17 @@
 from telethon import TelegramClient, events
 import re
+from tqdm import tqdm
 import asyncio
 from telethon.tl.custom import Button
-import PyBypass as bypasser
+import os
 import requests
 import json
 import sqlite3
-client = TelegramClient('anonn56ne2',1651836,"f8244276a17b5b2a711e7501857c8e55")
+from bot_keys import a,b,c,pdisk
+
+client = TelegramClient('sd2',a,b)
 url_regex = re.compile(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+')
 check_subscribe = []
-def callback(current, total):
-    global pbar
-    global prev_curr
-    pbar.update(current-prev_curr)
-    prev_curr = current
-def youtube_subscribe(chat_id):
-  conn = sqlite3.connect('youtube2.db')
-  cursor = conn.cursor()
-  cursor.execute('''CREATE TABLE IF NOT EXISTS mytable (id  TEXT)''')
-  #cursor.execute('SELECT id FROM mytable ORDER BY id DESC LIMIT 1')
-  ##last_value = cursor.fetchone()[0]
-  #print(last_value)
-  api_key = 'AIzaSyAQbEi082RXAQyXditWY5cCL-cjYTMQc9M'
-  youtube = build('youtube', 'v3', developerKey=api_key)
-  channel_id = 'UCrknCDE8hTnuYEBKgR84KVg'
-  channel_response = youtube.channels().list(part='statistics',id=channel_id).execute()
-  subscriber_count = channel_response['items'][0]['statistics']['subscriberCount']
-  print(subscriber_count)
-  cursor.execute(f"INSERT INTO mytable VALUES ({subscriber_count})")
-  conn.close()
-  #subscriber count---------------
-  conn = sqlite3.connect('youtube.db')
-  cursor = conn.cursor()
-  cursor.execute('''CREATE TABLE IF NOT EXISTS mytable (id  TEXT, value  TEXT,)''')
-  cursor.execute(f"SELECT COUNT(*) FROM mytable WHERE id ={chat_id}")
-  count = cursor.fetchone()[0]
-  conn.close
-  if count > 0:
-    print("id exist")
-  else:
-    conn = sqlite3.connect('mydatabase.db')
-    cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO mytable VALUES ({chat_id},0)")
-    conn.commit()
-    conn.close
-
-
 @client.on(events.CallbackQuery)
 async def handle_url_button(event):
     url = event.data
@@ -69,41 +35,37 @@ async def handler(event):
 @client.on(events.NewMessage())
 async def handler(event):
   dir(event)
- # prev_curr = 0
- # print(event)
-  #pbar = tqdm(total=1, unit='B', unit_scale=True)
+  print(event)
   links =event
-  #await client.send_message(chat,"🌠 downloading 🌠")
-  key = "509061uc94uwe1gl6aqq"
+  key = pdisk
   chat_id = event.chat_id
   a = requests.get(f"https://pdisk.pro/api/upload/server?key={key}").json()
   sess_id = a["sess_id"]
   print(a)
   result = a["result"]
-  await client.send_message(chat_id,"please wait.....")  
-  print(result)
-  path = await client.download_media(event.media)
-  print(path)
-  files = {
-    'sess_id': (None, f'{sess_id}'),
-    'utype': (None, 'prem'),
-    'file_0': open(f'{path}', 'rb'),
-}
-  response = requests.post(f'{result}', files=files).json()
-  c = response
-  c_json = json.dumps(str(c))
-  f = json.loads(c_json)
-  m= f.replace("{","").replace("}","").replace("[","").replace("]","").replace("'","")
-  cm_json = m.split("file_code")[1].split(",")[0].replace(":","")
-  print(f)
-  print(cm_json)
-  print("ok...file")
-  vbc = f"https://pdisk.pro/{cm_json}"
-  #pbar.close()
-  await client.send_message(chat_id,vbc.replace(" ",""))
-  #bypassed_link = bypasser.bypass(event.raw_text)
-  #await event.reply(bypassed_link)
+  try:
+    await client.send_message(chat_id,"please wait.....")  
+    print(result)
+    path = await client.download_media(event.media)
+    print(path)
+    files = {'sess_id': (None, f'{sess_id}'),'utype': (None, 'prem'),'file_0': open(f'{path}', 'rb'),}
+    response = requests.post(f'{result}', files=files).json()
+    c = response
+    c_json = json.dumps(str(c))
+    f = json.loads(c_json)
+    m= f.replace("{","").replace("}","").replace("[","").replace("]","").replace("'","")
+    cm_json = m.split("file_code")[1].split(",")[0].replace(":","")
+    print(f)
+    print(cm_json)
+    print("ok...file")
+    vbc = f"https://pdisk.pro/{cm_json}"
+    #pbar.close()
+    await client.send_message(chat_id,vbc.replace(" ",""))
+    os.remove(path)
+  except:
+    print("went wrong.....")
+
 print("bor_starting......")
-client.start(bot_token="5939463789:AAGXWKRX7w7uCXS-BREv_XiJ1ib5GjOa9bA")
+client.start(bot_token=c)
 print("bot_run_until_con......")
 client.run_until_disconnected()
