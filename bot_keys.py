a97e52b74b05f727ab42883f8d1b34eb4dd73d87
JAsuranbots <124456326+JAsuranbots@users.noreply.github.com>
2023-05-07 23:09:08 +0530

    Update bot_keys.py

--- bot_keys.py
+++ bot_keys.py
@@ -1,6 +1,6 @@
 import os
-a = int(os.environ["a"])
-b = os.environ["b"]
-c =os.environ["c"]
-pdisk =os.environ["pdisk"]
+a = int(1923471)
+b = "fcdc178451cd234e63faefd38895c991"
+c = "6008049923:AAGCAIycoXnr1bRd9amojOfJQWXgGOYswhU"
+pdisk = "2952vdrk2d6vuh68la29"
 
